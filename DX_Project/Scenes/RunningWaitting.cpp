0ad28a77af18acd39f5fd69b6e95dd83f265e5f4
sepyris <sepyris@gmail.com>
2024-08-18 15:23:46 +0900

    커밋 깜빡한 파일 추가

---
+++ DX_Project/Scenes/RunningWaitting.cpp
@@ -0,0 +1,289 @@
+#include "framework.h"
+
+RunningWaitting::RunningWaitting(float area)
+{
+	bg = new BackGroundUV(L"Texture/Image/RunningWait.png", Vector2(0, 0), Vector2(1, 1), Vector2(WIN_CENTER_X, WIN_CENTER_Y), Vector2(3, 3), 0.0f, Vector2(SCREEN_SIZE_X, SCREEN_SIZE_Y));
+	player = new AnimatePlayer(L"Texture/AnimateScene/Animation/sprite2.png");
+	CAM->SetTarget(player);
+	if (area == 1) {
+		player->pos = Vector2(-1190, 800);
+		CAM->pos = Vector2(-1270, 500);
+	}
+	else {
+		player->pos = Vector2(WIN_CENTER_X, WIN_CENTER_Y);
+	}
+	ground[0] = new RectCollider(Vector2(WIN_WIDTH * 3, 3));
+	ground[1] = new RectCollider(Vector2(755, 3));
+	ground[2] = new RectCollider(Vector2(120, 3));
+	ground[3] = new RectCollider(Vector2(440, 3));
+	ground[4] = new RectCollider(Vector2(110, 3));
+	ground[5] = new RectCollider(Vector2(405, 3));
+	ground[6] = new RectCollider(Vector2(110, 3));
+	ground[7] = new RectCollider(Vector2(110, 3));
+	ground[8] = new RectCollider(Vector2(595, 3));
+	ground[9] = new RectCollider(Vector2(80, 3));
+	ground[10] = new RectCollider(Vector2(80, 3));
+	ground[11] = new RectCollider(Vector2(80, 3));
+	ground[12] = new RectCollider(Vector2(80, 3));
+	ground[13] = new RectCollider(Vector2(80, 3));
+	ground[14] = new RectCollider(Vector2(80, 3));
+	ground[15] = new RectCollider(Vector2(975, 3));
+
+	ground[0]->pos = Vector2(Vector2(WIN_CENTER_X, 1245));
+	ground[1]->pos = Vector2(Vector2(-890, 1030));
+	ground[2]->pos = Vector2(Vector2(-420, 1030));
+	ground[3]->pos = Vector2(Vector2(-245, 645));
+	ground[4]->pos = Vector2(Vector2(455, 645));
+	ground[5]->pos = Vector2(Vector2(165, 590));
+	ground[6]->pos = Vector2(Vector2(525, 730));
+	ground[7]->pos = Vector2(Vector2(605, 810));
+	ground[8]->pos = Vector2(Vector2(860, 1138));
+	ground[9]->pos = Vector2(Vector2(1175, 1082));
+	ground[10]->pos = Vector2(Vector2(1250, 1027));
+	ground[11]->pos = Vector2(Vector2(1325, 972));
+	ground[12]->pos = Vector2(Vector2(1400, 917));
+	ground[13]->pos = Vector2(Vector2(1470, 862));
+	ground[14]->pos = Vector2(Vector2(1540, 807));
+	ground[15]->pos = Vector2(Vector2(2056, 752));
+	
+	left_col[0] = new RectCollider(Vector2(30, 55));
+	left_col[1] = new RectCollider(Vector2(30, 55));
+	left_col[2] = new RectCollider(Vector2(30, 55));
+	left_col[3] = new RectCollider(Vector2(30, 55));
+	left_col[4] = new RectCollider(Vector2(30, 55));
+	left_col[5] = new RectCollider(Vector2(30, 55));
+	left_col[6] = new RectCollider(Vector2(30, 55));
+	left_col[7] = new RectCollider(Vector2(30, 55));
+	left_col[0]->pos = Vector2(Vector2(-20, 617));
+	left_col[1]->pos = Vector2(Vector2(1150, 1110));
+	left_col[2]->pos = Vector2(Vector2(1225, 1055));
+	left_col[3]->pos = Vector2(Vector2(1300, 1000));
+	left_col[4]->pos = Vector2(Vector2(1370, 945));
+	left_col[5]->pos = Vector2(Vector2(1440, 890));
+	left_col[6]->pos = Vector2(Vector2(1515, 835));
+	left_col[7]->pos = Vector2(Vector2(1585, 780));
+
+	right_col[0] = new RectCollider(Vector2(30, 220));
+	right_col[0]->pos = Vector2(Vector2(-520, 1140));
+
+	ladder[0] = new RectCollider(Vector2(40, 340));
+	ladder[0]->pos = Vector2(Vector2(-415, 810));
+	ladder[1] = new RectCollider(Vector2(10, 180));
+	ladder[1]->pos = Vector2(Vector2(-385, 1115));
+	ladder[2] = new RectCollider(Vector2(10, 210));
+	ladder[2]->pos = Vector2(Vector2(-450, 1130));
+	ladder[3] = new RectCollider(Vector2(40, 310));
+	ladder[3]->pos = Vector2(Vector2(590, 955));
+
+	left_portal = new RectCollider(Vector2(60, 20));
+	left_portal->pos = Vector2(Vector2(-1190, 1020));
+
+	player->Update();
+	for (RectCollider* g : ground) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : left_col) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : right_col) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : hill_ground) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : ladder) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	left_portal->WorldUpdate();
+
+}
+
+RunningWaitting::~RunningWaitting()
+{
+
+	delete player;
+	delete bg;
+	for (RectCollider* g : ground) {
+		if (g != NULL) {
+			delete g;
+		}
+	}
+	for (RectCollider* g : left_col) {
+		if (g != NULL) {
+			delete g;
+		}
+	}
+	for (RectCollider* g : right_col) {
+		if (g != NULL) {
+			delete g;
+		}
+	}
+	for (RectCollider* g : hill_ground) {
+		if (g != NULL) {
+			delete g;
+		}
+	}
+	for (RectCollider* g : ladder) {
+		if (g != NULL) {
+			delete g;
+		}
+	}
+	delete left_portal;
+}
+
+void RunningWaitting::Update()
+{
+	if (player->pos.y > WIN_HEIGHT * 3) {
+		player->pos = Vector2(WIN_CENTER_X + 545, 500);
+	}
+	CAM->SetTarget(player);
+	bg->Update();
+
+	for (RectCollider* g : left_col) {
+		if (g != NULL) {
+			Vector2 collision;
+			if (player->GetCollider()->isCollision(g, &collision)) {
+				if (collision.x <= collision.y) {
+					if (player->pos.x < g->pos.x) {
+						player->pos.x -= collision.x * DELTA * 300.0f;
+					}
+				}
+			}
+		}
+	}
+	for (RectCollider* g : right_col) {
+		if (g != NULL) {
+			Vector2 collision;
+			if (player->GetCollider()->isCollision(g, &collision)) {
+				if (collision.x <= collision.y) {
+					if (player->pos.x > g->pos.x) {
+						player->pos.x += collision.x * DELTA * 300.0f;
+					}
+				}
+			}
+		}
+	}
+
+	for (RectCollider* g : ground) {
+		if (g != NULL) {
+			Vector2 collision;
+			if (player->GetCollider()->isCollision(g, &collision)) {
+				if (player->pos.y < g->pos.y) {
+					if (player->GetCollider()->BottomVX() > g->TopVX() - 1.0f) {
+						player->pos.y -= collision.y * DELTA * 20.0f;
+						player->landing();
+					}
+				}
+			}
+		}
+	}
+
+	for (RectCollider* g : hill_ground) {
+		if (g != NULL) {
+			if (player->GetCollider()->isCollision(g)) {
+				player->pos.y -= DELTA * 100.0f;
+				player->landing();
+			}
+		}
+	}
+	bool check_hanging = false;
+	for (RectCollider* g : ladder) {
+		if (g != NULL) {
+			Vector2 collision;
+			if (player->GetCollider()->isCollision(g, &collision)) {
+				if (player->IsHanging()) {
+					player->pos.x = g->GlobalPos().x;
+					check_hanging = true;
+				}
+			}
+		}
+	}
+	if (!check_hanging) {
+		player->SetIdle();
+	}
+
+	if (left_portal != NULL) {
+		if (player->GetCollider()->isCollision(left_portal)) {
+			if (KEY_DOWN(VK_UP)) {
+				program->CreateScene(2, 3);
+				return;
+			}
+		}
+	}
+
+	player->Update();
+	for (RectCollider* g : ground) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : left_col) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : right_col) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : hill_ground) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	for (RectCollider* g : ladder) {
+		if (g != NULL) {
+			g->WorldUpdate();
+		}
+	}
+	left_portal->WorldUpdate();
+}
+
+void RunningWaitting::Render()
+{
+	bg->Render();	
+	for (RectCollider* g : ground) {
+		if (g != NULL) {
+			g->Render();
+		}
+	}
+	for (RectCollider* g : left_col) {
+		if (g != NULL) {
+			g->Render();
+		}
+	}
+	for (RectCollider* g : right_col) {
+		if (g != NULL) {
+			g->Render();
+		}
+	}
+	for (RectCollider* g : hill_ground) {
+		if (g != NULL) {
+			g->Render();
+		}
+	}
+	for (RectCollider* g : ladder) {
+		if (g != NULL) {
+			g->Render();
+		}
+	}
+	left_portal->Render();
+	player->Render();
+}
+
+void RunningWaitting::PostRender()
+{
+	ImGui::SliderFloat2("p.pos", (float*)&player->pos, 0, WIN_WIDTH);
+	player->PostRender();
+}
---
+++ DX_Project/Scenes/RunningWaitting.h
@@ -0,0 +1,22 @@
+#pragma once
+class RunningWaitting :public Scene {
+private:
+	BackGroundUV* bg;
+	RectCollider* ground[20];
+	RectCollider* left_col[20];
+	RectCollider* right_col[20];
+	RectCollider* hill_ground[20];
+	RectCollider* ladder[20];
+	RectCollider* left_portal;
+	RectCollider* right_portal;
+
+	AnimatePlayer* player;
+public:
+	RunningWaitting(float area);
+	~RunningWaitting();
+
+	// Scene을(를) 통해 상속됨
+	virtual void Update() override;
+	virtual void Render() override;
+	virtual void PostRender() override;
+};
